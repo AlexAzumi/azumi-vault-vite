dab9133fa9023eb22e19af492ca6079ab175f6a0
Alejandro Suárez <alejandro-hdez115@outlook.com>
2025-04-07 20:51:21 -0600

    Add translations to the about me section

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,8 +1,11 @@
 import { FC } from 'react'
+import { useTranslation } from 'react-i18next'
 
 import profileImg from '../../assets/img/profile.jpg'
 
 const AboutMe: FC = () => {
+  const { t } = useTranslation()
+
   return (
     <div id='about-me' className='container mx-auto px-2 py-8 lg:px-0'>
       <div className='grid grid-cols-6 gap-4 px-4 md:px-2 xl:px-0'>
@@ -10,32 +13,24 @@ const AboutMe: FC = () => {
           <img src={profileImg} className='rounded shadow-lg' />
         </div>
         <div className='col-span-6 flex flex-col items-start justify-center text-xl text-white md:col-span-3 xl:col-span-2'>
-          <div className='mb-4 rounded bg-black p-4 shadow'>
-            ¡Hola!, mi nombre es{' '}
-            <div className='inline-block text-green-500'>Alejandro Suárez</div>.
-          </div>
-          <div className='mb-4 rounded bg-black p-4 shadow'>
-            Soy{' '}
-            <div className='inline-block text-green-500'>desarrollador web</div>{' '}
-            desde 2018.
-          </div>
-          <div className='mb-4 rounded bg-black p-4 shadow'>
-            Tengo más de dos años de experiencia laboral trabajando con{' '}
-            <div className='inline-block text-green-500'>React</div>,{' '}
-            <div className='inline-block text-green-500'>Tailwind</div>,{' '}
-            <div className='inline-block text-green-500'>Node.js</div> y{' '}
-            <div className='inline-block text-green-500'>TypeScript</div>.
-          </div>
-          <div className='rounded bg-black p-4 shadow'>
-            También tengo experiencia usando herramientas como{' '}
-            <div className='inline-block text-green-500'>Unity Engine</div>,{' '}
-            <div className='inline-block text-green-500'>React Native</div>,{' '}
-            <div className='inline-block text-green-500'>Git</div>,{' '}
-            <div className='inline-block text-green-500'>
-              bases de datos relacionales
-            </div>{' '}
-            y <div className='inline-block text-green-500'>C#</div>.
-          </div>
+          <div
+            className='mb-4 rounded bg-black p-4 shadow'
+            dangerouslySetInnerHTML={{
+              __html: t('aboutMe.firstText'),
+            }}
+          />
+          <div
+            className='mb-4 rounded bg-black p-4 shadow'
+            dangerouslySetInnerHTML={{ __html: t('aboutMe.secondText') }}
+          />
+          <div
+            className='mb-4 rounded bg-black p-4 shadow'
+            dangerouslySetInnerHTML={{ __html: t('aboutMe.thirdText') }}
+          />
+          <div
+            className='rounded bg-black p-4 shadow'
+            dangerouslySetInnerHTML={{ __html: t('aboutMe.fourthText') }}
+          />
         </div>
       </div>
     </div>
