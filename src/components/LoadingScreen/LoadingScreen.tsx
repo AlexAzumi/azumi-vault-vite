b3774958d4f99968edb5444d2bf51d9931153c4b
Alejandro Suárez <alejandro-hdez115@outlook.com>
2025-04-09 21:31:17 -0600

    Update loading screen component

--- src/components/LoadingScreen/LoadingScreen.tsx
+++ src/components/LoadingScreen/LoadingScreen.tsx
@@ -4,14 +4,14 @@ import { FC } from 'react'
 
 const LoadingScreen: FC = () => {
   return (
-    <div className='w-max-full text-whit flex h-screen w-screen flex-col items-center justify-center text-green-500'>
+    <div className='w-max-full text-whit flex h-screen w-screen flex-col items-center justify-center text-green-600'>
       <FontAwesomeIcon
         className='mb-6'
         icon={faSpinner}
         size='6x'
         spinPulse={true}
       />
-      <p className='text-3xl'>Loading...</p>
+      <p className='text-3xl font-extrabold'>Loading...</p>
     </div>
   )
 }
