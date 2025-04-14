8e1fef305b060bf28e3bd3747eab2900035a6726
Alejandro Suárez <alejandro-hdez115@outlook.com>
2025-04-14 15:09:48 -0600

    Add preview video in my projects

--- src/components/MyProjects/MyProjects.tsx
+++ src/components/MyProjects/MyProjects.tsx
@@ -3,8 +3,9 @@ import {
   faDiagramProject,
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { FC } from 'react'
+import { FC, useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next'
+import { TFunction } from 'i18next'
 
 import PROJECTS from '../../data/projects.json'
 
@@ -24,37 +25,95 @@ const MyProjects: FC = () => {
       </h1>
 
       {PROJECTS.map((item, idx) => (
-        <div
-          key={`project-${idx}`}
-          className='group grid grid-cols-1 px-4 last-of-type:mb-0 lg:mb-6 lg:grid-cols-2'
-        >
-          <div className='lg:translate-x-3 lg:group-even:order-2 lg:group-even:-translate-x-3'>
-            <img
-              src={item.cover}
-              className='aspect-video rounded drop-shadow-xl/50'
-            />
-          </div>
-          <div className='z-10 flex flex-col py-6 lg:-translate-x-3 lg:items-start lg:group-even:order-1 lg:group-even:translate-x-3 lg:group-even:items-end'>
-            <div className='mb-4 rounded bg-black p-4 lg:group-even:text-right'>
-              <h2 className='mb-4 text-3xl font-extrabold text-green-600'>
-                {item.name}
-              </h2>
-              <p
-                className='text-base/tight text-white'
-                dangerouslySetInnerHTML={{ __html: t(item.descriptionKey) }}
-              />
-            </div>
-            {item.url && (
-              <div className='bg-back group/link rounded bg-black text-center text-green-600'>
-                <a href={item.url} target='_blank' className='block p-4'>
-                  {item.url} <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
-                  <hr className='w-0 transition-all group-hover/link:w-full' />
-                </a>
-              </div>
-            )}
-          </div>
-        </div>
+        <ProjectItem key={`project-${idx}`} t={t} {...item} />
       ))}
+    </div>
+  )
+}
+
+interface ProjectItemProps {
+  name: string
+  cover: string
+  descriptionKey: string
+  url: string
+  videoUrl: string
+  t: TFunction<'translation', undefined>
+}
+
+const ProjectItem: FC<ProjectItemProps> = ({
+  name,
+  cover,
+  descriptionKey,
+  url,
+  videoUrl,
+  t,
+}) => {
+  const videoRef = useRef<HTMLVideoElement | null>(null)
+  const [videoPaused, setVideoPaused] = useState(true)
+
+  const togglePlayback = (play?: boolean) => {
+    if (!videoRef.current) return
+
+    if (videoRef.current.paused || play) {
+      videoRef.current.play()
+      setVideoPaused(false)
+    } else {
+      videoRef.current.pause()
+      setVideoPaused(true)
+    }
+  }
+
+  return (
+    <div className='group grid grid-cols-1 px-4 last-of-type:mb-0 lg:mb-6 lg:grid-cols-2'>
+      <div
+        className='group/thumbnail relative lg:translate-x-3 lg:group-even:order-2 lg:group-even:-translate-x-3'
+        {...(videoUrl
+          ? {
+              onMouseEnter: () => togglePlayback(true),
+              onMouseLeave: () => togglePlayback(false),
+              onClick: () => togglePlayback(true),
+            }
+          : null)}
+      >
+        <img
+          src={cover}
+          className={`aspect-video rounded opacity-100 transition-opacity ${!videoPaused && 'opacity-0!'}`}
+        />
+        <div
+          className={`absolute right-0 bottom-0 left-0 bg-black/30 p-3 text-white transition-opacity ${!videoPaused && 'opacity-0!'}`}
+        >
+          {t('myProjects.hoverForPreview')}
+        </div>
+        {videoUrl && (
+          <video
+            ref={videoRef}
+            className='absolute top-0 -z-10 rounded'
+            muted={true}
+            loop={true}
+          >
+            <source src={videoUrl} type='video/mp4' />
+          </video>
+        )}
+      </div>
+      <div className='z-10 flex flex-col py-6 lg:-translate-x-3 lg:items-start lg:group-even:order-1 lg:group-even:translate-x-3 lg:group-even:items-end'>
+        <div className='mb-4 rounded bg-black p-4 lg:group-even:text-right'>
+          <h2 className='mb-4 text-3xl font-extrabold text-green-600'>
+            {name}
+          </h2>
+          <p
+            className='text-base/tight text-white'
+            dangerouslySetInnerHTML={{ __html: t(descriptionKey) }}
+          />
+        </div>
+        {url && (
+          <div className='bg-back group/link rounded bg-black text-center text-green-600'>
+            <a href={url} target='_blank' className='block p-4'>
+              {url} <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+              <hr className='w-0 transition-all group-hover/link:w-full' />
+            </a>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
