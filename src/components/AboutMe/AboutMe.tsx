6b969028ea2311ddb7103229641edd55a32e0fcb
Alejandro Suárez <alejandro-hdez115@outlook.com>
2025-04-13 21:32:47 -0600

    Update title design

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -12,17 +12,17 @@ const AboutMe: FC = () => {
     <div className='container mx-auto my-8 flex min-h-screen flex-col lg:px-0'>
       <div id='about-me' className='-translate-y-26' />
 
-      <h1 className='text-shadow-xl relative mb-10 flex justify-center text-center text-2xl font-extrabold text-gray-900 uppercase md:text-3xl lg:text-4xl'>
-        <div className='z-10 flex items-center justify-center rounded bg-green-600 px-10 py-4'>
+      <h1 className='relative mb-10 flex justify-center text-center text-2xl font-extrabold text-gray-900 uppercase md:text-3xl lg:text-4xl'>
+        <div className='z-10 flex items-center justify-center rounded-full bg-green-600 px-10 py-4'>
           <FontAwesomeIcon icon={faIdCardClip} className='mr-4' />
           {t('aboutMe.title')}
         </div>
-        <hr className='absolute top-1/2 w-full rounded border-2 border-green-600' />
+        <hr className='absolute top-1/2 w-full translate-y-[-2px] rounded-full border-2 border-green-600' />
       </h1>
 
       <div className='grid grid-cols-6 gap-4 px-4 md:px-2 xl:px-0'>
         <div className='col-span-6 md:col-span-3 xl:col-span-2 xl:col-start-2'>
-          <img src={profileImg} className='rounded shadow-lg' />
+          <img src={profileImg} className='rounded drop-shadow-xl/50' />
         </div>
         <div className='col-span-6 flex flex-col items-start justify-center text-xl text-white md:col-span-3 xl:col-span-2'>
           <div
--- src/components/MyProjects/MyProjects.tsx
+++ src/components/MyProjects/MyProjects.tsx
@@ -15,12 +15,12 @@ const MyProjects: FC = () => {
     <div className='container mx-auto my-8 flex min-h-screen flex-col justify-center'>
       <div id='my-projects' className='-translate-y-26' />
 
-      <h1 className='text-shadow-xl relative mb-10 flex justify-center text-center text-2xl font-extrabold text-gray-900 uppercase md:text-3xl lg:text-4xl'>
-        <div className='z-10 flex items-center justify-center rounded bg-green-600 px-10 py-4'>
+      <h1 className='relative mb-10 flex justify-center text-center text-2xl font-extrabold text-gray-900 uppercase md:text-3xl lg:text-4xl'>
+        <div className='z-10 flex items-center justify-center rounded-full bg-green-600 px-10 py-4'>
           <FontAwesomeIcon icon={faDiagramProject} className='mr-4' />
           {t('myProjects.title')}
         </div>
-        <hr className='absolute top-1/2 w-full rounded border-2 border-green-600' />
+        <hr className='absolute top-1/2 w-full translate-y-[-2px] rounded-full border-2 border-green-600' />
       </h1>
 
       {PROJECTS.map((item, idx) => (
@@ -29,11 +29,14 @@ const MyProjects: FC = () => {
           className='group grid grid-cols-1 px-4 last-of-type:mb-0 lg:mb-6 lg:grid-cols-2'
         >
           <div className='lg:translate-x-3 lg:group-even:order-2 lg:group-even:-translate-x-3'>
-            <img src={item.cover} className='aspect-video rounded shadow-2xl' />
+            <img
+              src={item.cover}
+              className='aspect-video rounded drop-shadow-xl/50'
+            />
           </div>
           <div className='z-10 flex flex-col py-6 lg:-translate-x-3 lg:items-start lg:group-even:order-1 lg:group-even:translate-x-3 lg:group-even:items-end'>
             <div className='mb-4 rounded bg-black p-4 lg:group-even:text-right'>
-              <h2 className='mb-4 text-3xl font-extrabold text-green-600 shadow-2xl'>
+              <h2 className='mb-4 text-3xl font-extrabold text-green-600'>
                 {item.name}
               </h2>
               <p
